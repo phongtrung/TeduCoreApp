b094a8bd555d648a36161bb99f7efc0efa474d99
PhongTrung <phongnhan@gmail.com>
2018-07-01 22:28:09 +0700

    Bai 13

--- TeduCoreApp.Data.EF/AppDbContext.cs
+++ TeduCoreApp.Data.EF/AppDbContext.cs
@@ -1,12 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
 using TeduCoreApp.Data.EF.Configurations;
 using TeduCoreApp.Data.EF.Extensions;
 using TeduCoreApp.Data.Entities;
+using TeduCoreApp.Data.Interfaces;
 
 namespace TeduCoreApp.Data.EF
 {
@@ -19,6 +22,41 @@ namespace TeduCoreApp.Data.EF
 
         // Đặt tên các bảng để  gen ra CSDL
         public DbSet<Advertistment> Advertistments { get; set; }
+        public DbSet<Language> Languages { set; get; }
+        public DbSet<SystemConfig> SystemConfigs { get; set; }
+        public DbSet<Function> Functions { get; set; }
+
+        public DbSet<AppUser> AppUsers { get; set; }
+        public DbSet<AppRole> AppRoles { get; set; }
+        public DbSet<Announcement> Announcements { set; get; }
+        public DbSet<AnnouncementUser> AnnouncementUsers { set; get; }
+
+        public DbSet<Blog> Bills { set; get; }
+        public DbSet<BillDetail> BillDetails { set; get; }
+        public DbSet<Blog> Blogs { set; get; }
+        public DbSet<BlogTag> BlogTags { set; get; }
+        public DbSet<Color> Colors { set; get; }
+        public DbSet<Contact> Contacts { set; get; }
+        public DbSet<Feedback> Feedbacks { set; get; }
+        public DbSet<Footer> Footers { set; get; }
+        public DbSet<Page> Pages { set; get; }
+        public DbSet<Product> Products { set; get; }
+        public DbSet<ProductCategory> ProductCategories { set; get; }
+        public DbSet<ProductImage> ProductImages { set; get; }
+        public DbSet<ProductQuantity> ProductQuantities { set; get; }
+        public DbSet<ProductTag> ProductTags { set; get; }
+
+        public DbSet<Size> Sizes { set; get; }
+        public DbSet<Slide> Slides { set; get; }
+
+        public DbSet<Tag> Tags { set; get; }
+
+        public DbSet<Permission> Permissions { get; set; }
+        public DbSet<WholePrice> WholePrices { get; set; }
+
+        public DbSet<AdvertistmentPage> AdvertistmentPages { get; set; }
+
+        public DbSet<AdvertistmentPosition> AdvertistmentPositions { get; set; }
 
 
         protected override void OnModelCreating(ModelBuilder builder)
@@ -45,6 +83,29 @@ namespace TeduCoreApp.Data.EF
             
 
             base.OnModelCreating(builder);
+        }
+
+        public override int SaveChanges()
+        {
+            // Lấy các trạng thái chỉnh sửa hoặc thêm mới để tự động add vào ngày tạo mà ngày chỉnh sửa.
+            var modified = ChangeTracker.Entries().Where(e => e.State == EntityState.Modified || e.State == EntityState.Added);
+            foreach (EntityEntry item in modified)
+            {
+                var changedOrAddItem = item.Entity as IDateTracking;
+                // Nếu changedOrAddItem != null có nghĩa là item này có triển khai IDateTracking
+                if (changedOrAddItem != null)
+                {
+                    // Mỗi khi save sẽ tiến hành kiểm tra đê có thể tự kiểm tra để thêm vào thời gian tạo và chỉnh sữa mẫu tin.
+                    if (item.State == EntityState.Added)
+                    {
+                        changedOrAddItem.DateCreated  = DateTime.Now;
+                    }
+                    
+                    changedOrAddItem.DateModified = DateTime.Now;
+                }
+
+            }
+            return base.SaveChanges();
         }
     }
 }
--- TeduCoreApp/Startup.cs
+++ TeduCoreApp/Startup.cs
@@ -9,6 +9,8 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using TeduCoreApp.Data;
+using TeduCoreApp.Data.EF;
+using TeduCoreApp.Data.Entities;
 using TeduCoreApp.Models;
 using TeduCoreApp.Services;
 
@@ -26,21 +28,37 @@ namespace TeduCoreApp
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<ApplicationDbContext>(options =>
-                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+            // Mặc định.
+            //services.AddDbContext<ApplicationDbContext>(options =>
+            //    options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
 
-            services.AddIdentity<ApplicationUser, IdentityRole>()
-                .AddEntityFrameworkStores<ApplicationDbContext>()
+            // Sử dụng cấu hình tự tạo
+            services.AddDbContext<AppDbContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"), o => o.MigrationsAssembly("TeduCoreApp.Data.EF")));
+
+            // Mặc định
+            //services.AddIdentity<ApplicationUser, IdentityRole>()
+            //    .AddEntityFrameworkStores<ApplicationDbContext>()
+            //    .AddDefaultTokenProviders();
+
+            // Sử dụng cấu hình tự tạo
+            services.AddIdentity<AppUser, AppRole>()
+                .AddEntityFrameworkStores<AppDbContext>()
                 .AddDefaultTokenProviders();
 
+            // Khởi tạo UserMangager AppUser
+            services.AddScoped<UserManager<AppUser>, UserManager<AppUser>>();
+            services.AddScoped<RoleManager<AppRole>, RoleManager<AppRole>>();
             // Add application services.
             services.AddTransient<IEmailSender, EmailSender>();
+            // Khởi tạo một số dữ liệu có sẵn.. Menu,...
+            services.AddTransient<DbInitializer>();
 
             services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, DbInitializer dbInitializer)
         {
             if (env.IsDevelopment())
             {
@@ -63,6 +81,9 @@ namespace TeduCoreApp
                     name: "default",
                     template: "{controller=Home}/{action=Index}/{id?}");
             });
+
+            // Load các dữ liệu mẫu lên.
+            dbInitializer.Seed().Wait();
         }
     }
 }
