f589a95e8fa4034e84bc1666dc2588c2acfc6ce1
PhongTrung <phongnhan@gmail.com>
2018-07-01 23:31:15 +0700

    Bai 14 - 30%

---
+++ TeduCoreApp.Application/Implementation/ProductCategoryService.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TeduCoreApp.Application.Interfaces;
+using TeduCoreApp.Application.ViewModels.Product;
+
+namespace TeduCoreApp.Application.Implementation
+{
+    public class ProductCategoryService : IProductCategoryService
+    {
+        public ProductCategoryViewModel Add(ProductCategoryViewModel productCategoryVm)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<ProductCategoryViewModel> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<ProductCategoryViewModel> GetAll(string keyword)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<ProductCategoryViewModel> GetAllByParentId(int parentId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ProductCategoryViewModel GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<ProductCategoryViewModel> GetHomeCategories(int top)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ReOrder(int sourceId, int targetId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Save()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(ProductCategoryViewModel productCategoryVm)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateParentId(int sourceId, int targetId, Dictionary<int, int> items)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ TeduCoreApp.Data.EF/Repositories/ProductCategoryRepository.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TeduCoreApp.Data.Entities;
+using TeduCoreApp.Data.IRepositories;
+
+namespace TeduCoreApp.Data.EF.Repositories
+{
+    public class ProductCategoryRepository : EFRepository<ProductCategory, int>, IProductCategoryRepository
+    {
+        private AppDbContext _context;
+        public ProductCategoryRepository(AppDbContext context) : base(context)
+        {
+            _context = context;
+        }
+
+        public List<ProductCategory> GetByAlias(string alias)
+        {
+            return _context.ProductCategories.Where(x => x.SeoAlias == alias).ToList();
+        }
+    }
+}
